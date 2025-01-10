f52ba264c527a9e934ded84040af768fd2a5f939
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-10 23:40:12 +0530

    nav

---
+++ client/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="flex gap-6 py-7 ">
+        <ul>
+            <button>To-DO List</button>
+        </ul>
+
+        <ul className='flex gap-6'>
+            <li>Home</li>
+            <li>About</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
