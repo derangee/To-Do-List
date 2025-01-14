ae542e4f19ec58f9cdbe6eab251c1aeded5776af
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-15 00:01:42 +0530

    Update page.js

--- client/app/page.js
+++ client/app/page.js
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex">HI this is home page</div>
+    <div>
+      <div className="flex">
+      <input type="Enter your To-Do here" className="text-black"/>
+      <button>Add</button>
+      </div>
+    </div>
   );
 }
