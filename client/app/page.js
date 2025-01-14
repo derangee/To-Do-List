e20d256a5fd534ac3b486bd4701b3c054be27229
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-15 01:29:07 +0530

    Update page.js

--- client/app/page.js
+++ client/app/page.js
@@ -3,32 +3,46 @@ import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
 import EditIcon from '@mui/icons-material/Edit';
 
 export default function Home() {
+
+  const handleadd = () =>{
+
+  }
+
+  const handleedit = () =>{
+
+  }
+
+  const handledelete = () =>{
+
+  }
+
   return (
     <div className="text-gray-200">
 
     
       <div className="flex justify-center mt-7">
       <input type="text" placeholder="Enter your To-Do here" className="text-black px-4 py-2 rounded-md w-[35vw] bg-gray-200"/>
-      <button className="ml-5 text-lg px-4 text-bold rounded-md border-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-white transition duration-300" >  Add
-        </button>
+      <button onClick={handleadd()} className="ml-5 text-lg px-4 text-bold rounded-md border-2 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition duration-300" >Add</button>
       </div>
 
 
       <div className="task flex gap-7 justify-center">
-        <button className="text-xl   mt-[7vh] text-gray-400">Your Tasks</button>
-        <button className="text-xl   mt-[7vh] text-gray-400">Completed Tasks</button>
+        <button className="text-xl mt-[7vh] text-red-400 rounded-lg border-2 border-red-400 p-2 hover:bg-red-500 hover:text-white transition duration-300">Your Tasks</button>
+        <button className="text-xl mt-[7vh] text-green-400 rounded-lg border-2 border-green-400 p-2 hover:bg-green-500 hover:text-white transition duration-300">Completed Tasks</button>
       </div>
 
 
       <div className="flex justify-center">
-        <div className="container mt-5 p-4 flex justify-center rounded-lg border-2 border-green-500 bg-gray-800 w-[40vw] shadow-md gap-2">       
+        <div className="container mt-5 p-4 flex justify-center rounded-lg border-2 border-red-400 bg-gray-800 w-[40vw] shadow-md gap-2">       
         <span className="text-gray-300 w-[70%] break-words">Your To-Do</span>
         <div className="flex ml-[1vw] gap-4">
-        <button><EditIcon className="text-blue-500"></EditIcon> </button>
-        <button><DeleteRoundedIcon className="text-red-500"></DeleteRoundedIcon> </button>
+        <button onClick={handleedit()} ><EditIcon className="text-blue-500"></EditIcon> </button>
+        <button onClick={handledelete()}><DeleteRoundedIcon className="text-red-500"></DeleteRoundedIcon> </button>
         </div>      
         </div>
       </div>
+
+      
 
 
     </div>
