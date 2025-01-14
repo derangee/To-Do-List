bd11cc46fead51560cb2cd7db9cc863809cd998f
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-15 01:09:04 +0530

    Update page.js

--- client/app/page.js
+++ client/app/page.js
@@ -6,16 +6,19 @@ export default function Home() {
   return (
     <div className="text-gray-200">
 
-      {/*This is the input div*/}
+    
       <div className="flex justify-center mt-7">
       <input type="text" placeholder="Enter your To-Do here" className="text-black px-4 py-2 rounded-md w-[35vw] bg-gray-200"/>
       <button className="ml-5 text-lg px-4 text-bold rounded-md border-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-white transition duration-300" >  Add
         </button>
       </div>
 
-      <div className="task">
-        <h1 className="text-xl flex justify-center mt-[7vh] text-gray-400">Your Tasks</h1>
+
+      <div className="task flex gap-7 justify-center">
+        <button className="text-xl   mt-[7vh] text-gray-400">Your Tasks</button>
+        <button className="text-xl   mt-[7vh] text-gray-400">Completed Tasks</button>
       </div>
+
 
       <div className="flex justify-center">
         <div className="container mt-5 p-4 flex justify-center rounded-lg border-2 border-green-500 bg-gray-800 w-[40vw] shadow-md gap-2">       
