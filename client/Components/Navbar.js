5ec4eec1501133c98974fdae66574ed9b116bb70
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-14 19:00:46 +0530

    color added to entire screen

--- client/Components/Navbar.js
+++ client/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div>
       <div className=" text-xl flex gap-6 py-2 justify-between bg-gradient-to-r from-gray-800 via-gray-700 to-gray-600 text-white">
         <ul>
-            <button> <Link href="/">To-DO List</Link></button>
+            <button className='ml-7'> <Link href="/">To-DO List</Link></button>
         </ul>
 
         <ul className='text-xl flex gap-6 mr-[2vw]'>
--- client/app/page.js
+++ client/app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-screen">HI this is home page</div>
+    <div className="flex">HI this is home page</div>
   );
 }
