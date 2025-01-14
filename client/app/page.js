264c3a5db27998b7ddd7e5431ef3b530d8e1277a
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-15 00:14:41 +0530

    Update page.js

--- client/app/page.js
+++ client/app/page.js
@@ -3,9 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-gray-200">
-      <div className="flex justify-center">
-      <input type="text" placeholder="Enter your To-Do here" className="text-black px-4 py-2 rounded-md "/>
-      <button className="ml-5 text-lg">Add</button>
+      <div className="flex justify-center mt-7">
+      <input type="text" placeholder="Enter your To-Do here" className="text-black px-4 py-2 rounded-md w-[35vw] bg-gray-200"/>
+      <button className="ml-5 text-lg px-4 text-bold rounded-md border-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-white transition duration-300" >  Add
+        </button>
       </div>
     </div>
   );
