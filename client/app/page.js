28963e16e91a22a9dd7ad441167e2b1b9d5d1c12
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-28 13:46:16 +0530

    Update page.js

--- client/app/page.js
+++ client/app/page.js
@@ -21,28 +21,14 @@ export default function Home() {
   return (
     <div className="text-gray-200">
 
-    
-      <div className="flex justify-center mt-12">
-      <input type="text" placeholder="Enter your To-Do here" className="text-black px-4 py-2 rounded-md w-[35vw] bg-gray-200"/>
-      <button onClick={handleadd} className="ml-5 text-lg px-4 text-bold rounded-md border-2 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition duration-300" >Add</button>
-      </div>
+  <div className="box border-white text-center">
 
+  <div className="inp flex gap-7 justify-center">
+   <input type="input form" placeholder="Enter your To-Do task here"  className="w-[40%] text-black rounded-3xl h-11 " />
+   <button className="text-3xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">Add</button>
+   </div>
 
-      <div className="task flex gap-7 justify-center">
-        <button className="text-lg mt-[7vh] text-red-400 rounded-md border-2 border-red-400 p-2 hover:bg-red-500 hover:text-white transition duration-300">Your To-Do</button>
-        <button className="text-lg mt-[7vh] text-green-400 rounded-md border-2 border-green-400 p-2 hover:bg-green-500 hover:text-white transition duration-300">Completed Tasks</button>
-      </div>
-
-
-      <div className="flex justify-center">
-        <div className="container mt-5 p-4 flex justify-center rounded-lg border-2 border-red-400 w-[40vw] shadow-md gap-2">       
-        <span className="text-gray-300 w-[70%] break-words">Your To-Dos</span>
-        <div className="flex ml-[1vw] gap-4">
-        <button onClick={handleedit} ><EditIcon className="text-blue-500"></EditIcon> </button>
-        <button onClick={handledelete}> <DeleteRoundedIcon className="text-red-500"></DeleteRoundedIcon> </button>
-        </div>      
-        </div>
-      </div>
+  </div>
 
       
 
