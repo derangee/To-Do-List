5d8746d07b930d99954fbde097962a19a6f95787
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-28 14:14:46 +0530

    Update page.js

--- client/app/page.js
+++ client/app/page.js
@@ -21,15 +21,21 @@ export default function Home() {
   return (
   <div className="">
 
-  <div className="box border border-white w-[80%] mx-auto text-center mt-20 h-full">
+  <div className="box border border-white w-[80%] mx-auto text-center mt-20 h-full rounded-2xl">
 
  
-  <div className="input flex gap-7 justify-center ">
-   <input type="input form" placeholder="Enter your To-Do task here"  className="w-[40%] text-black rounded-3xl h-11 p-2  " />
+  <div className="input flex gap-7 justify-center mt-7 ">
+   <input type="input form" placeholder="Enter your To-Do task here"  className="w-[40%] text-black rounded-3xl h-11 p-2 font-bold" />
    <button onClick={handleadd} className="text-3xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">Add</button>
   </div>
 
-  
+  <div className="card border border-white w-[60%] rounded-3xl mx-auto mt-7 mb-11 flex justify-between p-4"> 
+    <div className="max-w-[75%] flex-col"> Your todo</div>
+    <div className="icons flex gap-4"> 
+     <button><EditIcon className="text-blue-500"/></button> 
+     <button><DeleteRoundedIcon className="text-red-500"/></button>
+    </div>
+  </div>
 
    
 
