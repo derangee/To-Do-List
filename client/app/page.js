6159639341b17f1d77d978172a1f0fe6516cc253
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-28 14:19:32 +0530

    Update page.js

--- client/app/page.js
+++ client/app/page.js
@@ -30,10 +30,10 @@ export default function Home() {
   </div>
 
   <div className="card border border-white w-[60%] rounded-3xl mx-auto mt-7 mb-11 flex justify-between p-4"> 
-    <div className="max-w-[75%] flex-col"> Your todo</div>
+    <div className="max-w-[75%] flex-col text-xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500"> Your todo</div>
     <div className="icons flex gap-4"> 
-     <button><EditIcon className="text-blue-500"/></button> 
-     <button><DeleteRoundedIcon className="text-red-500"/></button>
+     <button><EditIcon className="text-blue-500 text-2xl"/></button> 
+     <button><DeleteRoundedIcon className="text-red-500 text-2xl"/></button>
     </div>
   </div>
 
