1f49b25256262c52c764f34070793efb1d26920e
derangee <daksh_vashishtha@srmap.edu.in>
2025-06-18 16:58:41 +0530

    final commit ✅

--- client/app/about/page.js
+++ client/app/about/page.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const about = () => {
   return (
-    <div>
-      about page
+    <div className='mt-7 text-lg font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500'>
+      Manadatory learning project - todo list with basic features like add, delete and edit.
     </div>
   )
 }
